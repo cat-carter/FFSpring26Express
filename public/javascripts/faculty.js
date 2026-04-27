000a08db85ec425af78c9f1561bc8429723411c5
Catherine Carter <cvcarter@miami.edu>
2026-04-27 21:07:26 +0000

    made layout changes to submission page

--- public/javascripts/faculty.js
+++ public/javascripts/faculty.js
@@ -40,3 +40,8 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+function updateFilename(inputId, spanId) {
+  const input = document.getElementById(inputId);
+  const span = document.getElementById(spanId);
+  span.textContent = input.files.length > 0 ? input.files[0].name : 'No file chosen';
+}
